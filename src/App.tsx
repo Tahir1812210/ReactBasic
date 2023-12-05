1ddb22234ba1496cf718303ac372e8fa0afb0628
Tahirid1812210 <cs1812210@szabist.pk>
2023-12-05 15:54:32 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,55 @@
-import { useState } from 'react';
-import Alert from './components/Alert';
-import Buttons from './components/Buttons/Buttons';
+import React, { useState } from 'react';
 import ListGroup from './components/ListGroup';
 import { BsFillCalendarFill } from 'react-icons/bs';
 import Like from './components/Like';
 
 function App() {
-  let items = ['New York', 'San Fransisco', 'Tokyo', 'London', 'Paris'];
+  // const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
 
-  const [alertVisible, setAlertVisible] = useState(false);
+  // const [game, setGame] = useState({
+  //   id: 1,
+  //   player: {
+  //     name: 'John',
+  //   },
+  // });
 
-  // const colors = [
-  //   'btn-primary',
-  //   'btn-secondary',
-  //   'btn-success',
-  //   'btn-danger',
-  //   'btn-warning',
-  //   'btn-info',
-  //   'btn-light',
-  //   'btn-dark',
-  //   'btn-link',
-  // ];
+  // const handleClick = () => {
+  //   setGame({ ...game, player: { ...game.player, name: 'Bob' } });
+  // };
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
+  // const [pizza, setPizza] = useState({
+  //   name: 'Spicy Pepproni',
+  //   toppings: ['Mushrooms'],
+  // });
+
+  // const handleClick = () => {
+  //   setPizza({ ...pizza, toppings: [...pizza.toppings, 'Cheese'] });
+  // };
+
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: 'Product 1', quantity: 1 },
+      { id: 2, title: 'Product 2', quantity: 1 },
+    ],
+  });
+
+  const handleClick = () => {
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      <BsFillCalendarFill color="red" size="40" />
-      <ListGroup
-        items={items}
-        headings="Cities"
-        onSelectItem={handleSelectItem}
-      />
-      {/* <Alert>
-        Hello <span> World </span>
-      </Alert> */}
-      {/* {alertVisible && (
-        <Alert onClose={() => setAlertVisible(false)}>My Alert</Alert>
-      )} */}
-
-      <Like onClick={() => console.log('clicked')} />
-
-      <Buttons color="primary" onClick={() => setAlertVisible(true)}>
-        My Button
-      </Buttons>
+      {/* <BsFillCalendarFill color="red" size="40" /> */}
+      {/* <ListGroup items={items} headings="Cities" /> */}
+      {/* <Like onClick={() => console.log('clicked')} /> */}
+      {/* <button onClick={() => setAlertVisible(true)}>My Button</button> */}
+      {/* <button onClick={handleClick}>Show</button> */}
     </div>
   );
 }
